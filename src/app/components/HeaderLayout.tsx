7d377f99fa4e0200c1964152050c61d37c4a4092
boyaneck <jinxx93@naver.com>
2024-05-13 02:07:58 +0900

    [add] 회원가입/로그인 모달 완성

--- src/app/components/HeaderLayout.tsx
+++ src/app/components/HeaderLayout.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import LoginModal from "./LoginModal";
 
 const HeaderLayout = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <header className="flex items-center bg-white h-16 border-b px-5 lg:px-8 z-5">
       <a className="font-bold text-2xl" href="/">
@@ -19,9 +22,21 @@ const HeaderLayout = () => {
         >
           회원가입
         </a>{" "}
-        <button className="text-[15px] font-medium text-gray-800 hover:text-black transition">
+        {/* <button className="text-[15px] font-medium text-gray-800 hover:text-black transition">
+          로그인
+        </button> */}
+        <button
+          className="text-[15px] font-medium text-grey-800 hover:cursor-pointer hover:text-black transition hover:font-bold"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
           로그인
         </button>
+        <LoginModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
       </div>
     </header>
   );
